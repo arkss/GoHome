5632589f3c4ca5622962a9ecfb12f6b7a4329a9f
CarrotNuts <kain6245@gmail.com>
2020-04-08 23:28:53 +0900

    Rename MainActivity, Add LoginActivity

---
+++ FrontEnd/GoHome/app/src/main/java/com/example/gohome/LoginActivity.java
@@ -0,0 +1,25 @@
+package com.example.gohome;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle saveInstanceState) {
+        super.onCreate(saveInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button signInBtn = (Button)findViewById(R.id.login_signIn);
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MapActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- FrontEnd/GoHome/app/src/main/java/com/example/gohome/MapActivity.java
+++ FrontEnd/GoHome/app/src/main/java/com/example/gohome/MapActivity.java
@@ -17,7 +17,8 @@ public class MapActivity extends AppCompatActivity {
         // 티맵 view 생성
         LinearLayout linearLayoutTmap = (LinearLayout)findViewById(R.id.linearLayoutTmap);
         TMapView tMapView = new TMapView(this);
-        tMapView.setSKTMapApiKey("l7xx696f792e752c433bacdc8fa3b644bf9b");
+//        tMapView.setSKTMapApiKey("l7xx696f792e752c433bacdc8fa3b644bf9b");
+        tMapView.setSKTMapApiKey("l7xx7574967eec1847a08c21f9d5c78980d4"); // 찬표 api key
         linearLayoutTmap.addView(tMapView);
     }
 }
