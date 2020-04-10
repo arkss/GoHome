fb07fbf0e0cbb9235badaa2ae075c12eedc6d26d
CarrotNuts <kain6245@gmail.com>
2020-04-10 17:11:51 +0900

    Add drawer layout, Modify map layout

--- FrontEnd/GoHome/app/src/main/java/com/example/gohome/LoginActivity.java
+++ FrontEnd/GoHome/app/src/main/java/com/example/gohome/LoginActivity.java
@@ -19,6 +19,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(LoginActivity.this, MapActivity.class);
                 startActivity(intent);
+                finish();
             }
         });
     }
--- FrontEnd/GoHome/app/src/main/java/com/example/gohome/MapActivity.java
+++ FrontEnd/GoHome/app/src/main/java/com/example/gohome/MapActivity.java
@@ -1,13 +1,19 @@
 package com.example.gohome;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.skt.Tmap.TMapView;
 
 public class MapActivity extends AppCompatActivity {
+    final DrawerLayout drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+    final View drawerView = (View)findViewById(R.id.drawer);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,15 @@ public class MapActivity extends AppCompatActivity {
 //        tMapView.setSKTMapApiKey("l7xx696f792e752c433bacdc8fa3b644bf9b");
         tMapView.setSKTMapApiKey("l7xx7574967eec1847a08c21f9d5c78980d4"); // 찬표 api key
         linearLayoutTmap.addView(tMapView);
+
+        // search bar의 menu버튼 클릭시 map_drawer 레이아웃 열림
+        ImageButton sMenuBtn = (ImageButton) findViewById(R.id.searchbar_menu);
+        sMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawerLayout.openDrawer(drawerView, true);
+            }
+        });
+
     }
 }
