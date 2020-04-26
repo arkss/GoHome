f43e0e7760c999583af1604600718ac6a430f797
Geonho Kim <gunhoflash@gmail.com>
2020-04-26 15:44:19 +0900

    Add an util function for json

--- BackEnd/Node/controllers/util/_json.js
+++ BackEnd/Node/controllers/util/_json.js
@@ -1,10 +1,28 @@
-exports.unwrap_properties = (json) => {
-	for (let property in json) {
-		if (Array.isArray(json[property]) && json[property].length == 1) {
-			json[property] = json[property][0];
+exports.unwrap_properties = (jsons) => {
+	let ar = (Array.isArray(jsons)) ? jsons : [jsons];
+
+	for (let json of ar) {
+		for (let property in json) {
+			if (Array.isArray(json[property]) && json[property].length == 1) {
+				json[property] = json[property][0];
+			}
 		}
 	}
-	return json;
+
+	return jsons;
+};
+
+exports.delete_properties = (jsons, properties) => {
+	let ar_jsons = (Array.isArray(jsons)) ? jsons : [jsons];
+	let ar_properties = (Array.isArray(properties)) ? properties : [properties];
+
+	for (let json of ar_jsons) {
+		for (let property of ar_properties) {
+			delete json[property];
+		}
+	}
+
+	return jsons;
 };
 
 exports.get_value = (json, default_value, ...properties) => {
