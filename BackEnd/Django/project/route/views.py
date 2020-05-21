7ff0b4b3d3e7ddbe599796fd90e971112e622987
minsung <rkdalstjd9@naver.com>
2020-05-21 12:07:38 +0900

    route test 성공

--- BackEnd/Django/project/route/views.py
+++ BackEnd/Django/project/route/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.response import Response
+import requests
 
-# Create your views here.
+
+@api_view(['GET'])
+@permission_classes([AllowAny, ])
+def route_test(request):
+    r = requests.get('http://15.164.105.118/')
+    # r = requests.get('https://api.github.com/events')
+    print(r.json())
+    print(r)
+    breakpoint()
+    print(r)
+    return Response({
+        'test': 'success'
+    })
