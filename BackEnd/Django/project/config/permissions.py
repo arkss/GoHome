1a254d54d68a9850015b45bd7acbb334ce63b78f
minsung <rkdalstjd9@naver.com>
2020-05-27 23:59:25 +0900

    MyIsAuthenticated 변경

--- BackEnd/Django/project/config/permissions.py
+++ BackEnd/Django/project/config/permissions.py
@@ -5,15 +5,16 @@ class MyIsAuthenticated(permissions.BasePermission):
     message = ''
 
     def has_permission(self, request, view):
-        if not bool(request.user):
+        if request.user.is_anonymous:
             self.message = '유저가 존재하지 않습니다.'
+            return False
+
+        elif bool(request.user.status == '0'):
+            self.message = '이메일 인증이 필요합니다.'
             return False
 
         elif not bool(request.user.is_authenticated):
             self.message = '로그인이 필요합니다.'
             return False
 
-        elif bool(request.user.status == '0'):
-            self.message = '이메일 인증이 필요합니다.'
-            return False
         return True
