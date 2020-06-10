1a129af18631d99e38285170631a1d9feb462b3e
yonsweng <39218496+yonsweng@users.noreply.github.com>
2020-06-10 17:22:24 +0900

    Define RouteSearch Data

--- FrontEnd/GoHome/app/src/main/java/com/example/gohome/retrofit2/RouteSearchQuery.java
+++ FrontEnd/GoHome/app/src/main/java/com/example/gohome/retrofit2/RouteSearchQuery.java
@@ -4,62 +4,72 @@ import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
 
 public class RouteSearchQuery {
-    @SerializedName("result")
+
+    @SerializedName("lat_start")
     @Expose
-    private int result;
-    @SerializedName("message")
+    private double latStart;
+    @SerializedName("lon_start")
     @Expose
-    private String message;
-    @SerializedName("data")
+    private double lonStart;
+    @SerializedName("lat_end")
     @Expose
-    private RouteData data;
-
-    public int getResult() {
-        return result;
-    }
-
-    public void setResult(int result) {
-        this.result = result;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-    public RouteData getData() {
-        return data;
-    }
-
-    public void setData(RouteData data) {
-        this.data = data;
-    }
-}
-
-class RouteData {
-    @SerializedName("n")
+    private double latEnd;
+    @SerializedName("lon_end")
     @Expose
-    private int n;
-    @SerializedName("routes")
+    private double lonEnd;
+    @SerializedName("include_bike")
     @Expose
-    private Route[] routes = null;
+    private String includeBike;
+    @SerializedName("include_bus")
+    @Expose
+    private String includeBus;
 
-    public int getN() {
-        return n;
+    public double getLatStart() {
+        return latStart;
     }
 
-    public void setN(int n) {
-        this.n = n;
+    public void setLatStart(double latStart) {
+        this.latStart = latStart;
     }
 
-    public Route[] getRoutes() {
-        return routes;
+    public double getLonStart() {
+        return lonStart;
     }
 
-    public void setRoutes(Route[] routes) {
-        this.routes = routes;
+    public void setLonStart(double lonStart) {
+        this.lonStart = lonStart;
     }
+
+    public double getLatEnd() {
+        return latEnd;
+    }
+
+    public void setLatEnd(double latEnd) {
+        this.latEnd = latEnd;
+    }
+
+    public double getLonEnd() {
+        return lonEnd;
+    }
+
+    public void setLonEnd(double lonEnd) {
+        this.lonEnd = lonEnd;
+    }
+
+    public String getIncludeBike() {
+        return includeBike;
+    }
+
+    public void setIncludeBike(String includeBike) {
+        this.includeBike = includeBike;
+    }
+
+    public String getIncludeBus() {
+        return includeBus;
+    }
+
+    public void setIncludeBus(String includeBus) {
+        this.includeBus = includeBus;
+    }
+
 }
