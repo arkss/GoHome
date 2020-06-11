593254c483b325a352179e61d3e796df5c067438
yonsweng <39218496+yonsweng@users.noreply.github.com>
2020-06-11 11:33:27 +0900

    첫 화면 경로 없애기

--- FrontEnd/GoHome/app/src/main/java/com/example/gohome/main/MapFragment.java
+++ FrontEnd/GoHome/app/src/main/java/com/example/gohome/main/MapFragment.java
@@ -122,21 +122,20 @@ public class MapFragment extends Fragment implements View.OnClickListener {
         LinearLayout linearLayoutTmap = getView().findViewById(R.id.linearLayoutTmap);
         linearLayoutTmap.addView(tMapView);
 
-        TMapPoint point1 = new TMapPoint(37.583, 127.054);
-        TMapPoint point2 = new TMapPoint(37.580, 127.047);
-        ArrayList<TMapPoint> list = new ArrayList<>();
-        TMapData tmapdata = new TMapData();
-        tmapdata.findPathDataWithType(TMapData.TMapPathType.CAR_PATH, point1, point2, new TMapData.FindPathDataListenerCallback() {
-            @Override
-            public void onFindPathData(TMapPolyLine polyLine) {
-                tMapView.addTMapPath(polyLine);
-                ArrayList<TMapPoint> t_list = polyLine.getLinePoint();
-                for(TMapPoint point : t_list) {
-                    Log.d("MapFragment", "l: "+point.getLongitude() + ", l: "+point.getLatitude());
-                }
-            }
-        });
-
+//        TMapPoint point1 = new TMapPoint(37.583, 127.054);
+//        TMapPoint point2 = new TMapPoint(37.580, 127.047);
+//        ArrayList<TMapPoint> list = new ArrayList<>();
+//        TMapData tmapdata = new TMapData();
+//        tmapdata.findPathDataWithType(TMapData.TMapPathType.CAR_PATH, point1, point2, new TMapData.FindPathDataListenerCallback() {
+//            @Override
+//            public void onFindPathData(TMapPolyLine polyLine) {
+//                tMapView.addTMapPath(polyLine);
+//                ArrayList<TMapPoint> t_list = polyLine.getLinePoint();
+//                for(TMapPoint point : t_list) {
+//                    Log.d("MapFragment", "l: "+point.getLongitude() + ", l: "+point.getLatitude());
+//                }
+//            }
+//        });
 
         shareBtn = (FloatingActionButton) view.findViewById(R.id.position_share_btn);
         shareBtn.setOnClickListener(this);
