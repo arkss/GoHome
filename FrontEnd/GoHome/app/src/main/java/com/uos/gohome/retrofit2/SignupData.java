297e30b8be30ac5d963c482fb842f1b04b286437
CarrotNuts <kain6245@gmail.com>
2020-06-15 10:09:04 +0900

    Modify Signup

--- FrontEnd/GoHome/app/src/main/java/com/uos/gohome/retrofit2/SignupData.java
+++ FrontEnd/GoHome/app/src/main/java/com/uos/gohome/retrofit2/SignupData.java
@@ -6,8 +6,8 @@ public class SignupData {
     @SerializedName("profile")
     private SignupProfile profile;
 
-    public SignupData(String username, String password, String email, String nickname) {
-        this.profile = new SignupProfile(username, password, email, nickname);
+    public SignupData(String username, String password, String email, String nickname, String address, String detail_address) {
+        this.profile = new SignupProfile(username, password, email, nickname, address, detail_address);
     }
 
     public SignupProfile getProfile() {
@@ -27,12 +27,18 @@ public class SignupData {
         private String email;
         @SerializedName("nickname")
         private String nickname;
+        @SerializedName("address")
+        private String address;
+        @SerializedName("detail_address")
+        private String detail_address;
 
-        public SignupProfile(String username, String password, String email, String nickname) {
+        public SignupProfile(String username, String password, String email, String nickname, String address, String detail_address) {
             this.username = username;
             this.password = password;
             this.email = email;
             this.nickname = nickname;
+            this.address = address;
+            this.detail_address = detail_address;
         }
 
         public String getUsername() {
@@ -65,6 +71,22 @@ public class SignupData {
 
         public void setNickname(String nickname) {
             this.nickname = nickname;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public String getDetail_address() {
+            return detail_address;
+        }
+
+        public void setAddress(String address) {
+            this.address = address;
+        }
+
+        public void setDetail_address(String detail_address) {
+            this.detail_address = detail_address;
         }
     }
 }
