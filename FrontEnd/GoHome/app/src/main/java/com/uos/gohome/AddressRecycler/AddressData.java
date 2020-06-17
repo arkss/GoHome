d8368424c397b26599e73c88215b817eb5fc7142
yonsweng <39218496+yonsweng@users.noreply.github.com>
2020-06-17 12:46:38 +0900

    주소 검색 결과 표시

--- FrontEnd/GoHome/app/src/main/java/com/uos/gohome/AddressRecycler/AddressData.java
+++ FrontEnd/GoHome/app/src/main/java/com/uos/gohome/AddressRecycler/AddressData.java
@@ -2,9 +2,13 @@ package com.uos.gohome.AddressRecycler;
 
 public class AddressData {
     private String address;
+    private double latitude;
+    private double longitude;
 
-    public AddressData(String address) {
+    public AddressData(String address, double latitude, double longitude) {
         this.address = address;
+        this.latitude = latitude;
+        this.longitude = longitude;
     }
 
     public String getAddress() {
@@ -13,5 +17,21 @@ public class AddressData {
 
     public void setAddress(String address) {
         this.address = address;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
     }
 }
--- FrontEnd/GoHome/app/src/main/java/com/uos/gohome/AddressSearch.java
+++ FrontEnd/GoHome/app/src/main/java/com/uos/gohome/AddressSearch.java
@@ -7,16 +7,33 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.skt.Tmap.TMapData;
+import com.skt.Tmap.TMapPOIItem;
+import com.skt.Tmap.TMapView;
 import com.uos.gohome.AddressRecycler.AddressData;
 import com.uos.gohome.AddressRecycler.AddressRecyclerAdapter;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class AddressSearch extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private LinearLayoutManager layoutManager;
+    ArrayList<AddressData> addressDataList;
+
+    TMapView tMapView;
+    TMapData tmapdata;
+
+    Handler notifyHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +41,8 @@ public class AddressSearch extends AppCompatActivity {
         setContentView(R.layout.activity_address_search);
 
         // RecyclerView에 표시할 데이터 리스트 생성.
-        ArrayList<AddressData> addressDataList = new ArrayList<AddressData>();
-        addressDataList.add(new AddressData("가"));
-        addressDataList.add(new AddressData("나"));
-        addressDataList.add(new AddressData("다"));
+        addressDataList = new ArrayList<AddressData>();
+        addressDataList.add(new AddressData("가", 0, 0));
 
         recyclerView = (RecyclerView)findViewById(R.id.address_recycler_view);
 
@@ -48,6 +63,27 @@ public class AddressSearch extends AppCompatActivity {
         mAdapter = new AddressRecyclerAdapter(addressDataList);
         recyclerView.setAdapter(mAdapter);
 
+        tMapView = new TMapView(this);
+        tMapView.setSKTMapApiKey("l7xx7574967eec1847a08c21f9d5c78980d4"); // 찬표 api key
+        tmapdata = new TMapData();
+
+        // 버튼 누르면 주소 검색
+        Button searchButton = (Button)findViewById(R.id.address_search_button);
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText editTextPOI = (EditText)findViewById(R.id.editTextPOI);
+                String text = editTextPOI.getText().toString();
+                getAddresses(text);  // update addressDataList
+            }
+        });
+
+        notifyHandler = new Handler(){
+            public void handleMessage(Message msg){
+                mAdapter.notifyDataSetChanged();
+            }
+        };
+
 //        addressDataList.add(new AddressData("나"));
 //        mAdapter.notifyDataSetChanged();
 //
@@ -60,5 +96,24 @@ public class AddressSearch extends AppCompatActivity {
 //        data.putExtra("address", "서울시립대학교");
 //        setResult(RESULT_OK, data);
 //        finish();
+    }
+
+    void getAddresses(String text) {
+        addressDataList.clear();
+        tmapdata.findAllPOI(text, 10, new TMapData.FindAllPOIListenerCallback() {
+            @Override
+            public void onFindAllPOI(ArrayList poiItem) {
+                // 모든 주소, 위도 경도 저장.
+                for(int i = 0; i < poiItem.size(); i++) {
+                    TMapPOIItem item = (TMapPOIItem)poiItem.get(i);
+                    String address = item.getPOIAddress().replace(" null", "") + " " + item.getPOIName();
+                    double latitude = item.getPOIPoint().getLatitude();
+                    double longitude = item.getPOIPoint().getLongitude();
+                    addressDataList.add(new AddressData(address, latitude, longitude));
+                }
+                Message msg = notifyHandler.obtainMessage();
+                notifyHandler.sendMessage(msg);
+            }
+        });
     }
 }
