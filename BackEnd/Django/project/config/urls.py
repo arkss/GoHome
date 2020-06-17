c2e7f9aeadbefac1758e522127adc02e07641aa4
minsung <rkdalstjd9@naver.com>
2020-06-18 00:39:09 +0900

    노드 서버와의 연동

--- BackEnd/Django/project/config/urls.py
+++ BackEnd/Django/project/config/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    path('route/', include('route.urls')),
+    path('api/', include('route.urls')),
     path('share/', include('share.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
