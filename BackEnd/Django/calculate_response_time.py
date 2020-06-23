56bfc9a0186bcaa9df7b96aa5496aac97516b3a0
minsung <rkdalstjd9@naver.com>
2020-06-23 21:07:25 +0900

    여러 API에 대한 테스트

--- BackEnd/Django/calculate_response_time.py
+++ BackEnd/Django/calculate_response_time.py
@@ -23,12 +23,32 @@ headers = {
 }
 
 total_time = 0
-request_cnt = 1000
+request_cnt = 100
+
+bikestops_URL = f'{URL}api/bikestops/'
+bikestop_parked_counts_URL = f'{URL}api/bikestop_parked_counts/'
+routes_URL = f'{URL}api/routes/'
+routes_params = {
+    'lat_start': 37.586759,
+    'lon_start': 127.053907,
+    'lat_end': 37.582931,
+    'lon_end': 127.060980
+}
 
 for i in range(request_cnt):
     start = time.time()
-    response = requests.get(f'{URL}share/1/position/', headers=headers)
+    params = {
+        'lat': 37.58396981971035,
+        'lon': 127.05907344818158,
+        'n': 5
+    }
+    response = requests.get(
+        routes_URL,
+        headers=headers,
+        params=routes_params
+    )
     end = time.time()
     total_time += end - start
 
 print(total_time/request_cnt)
+breakpoint()
